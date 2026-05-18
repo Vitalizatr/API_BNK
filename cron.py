5f15b3b73c6fc3e10529ed32e6b29a444c7c3c26
Владислав <vladislav@MacBook-Air-Vladislav.local>
2026-05-18 20:40:47 +0300

    Add workflow

--- cron.py
+++ cron.py
@@ -1,4 +1,5 @@
 import os
+import json
 from parser import Parser
 from dotenv import load_dotenv
 
@@ -12,7 +13,11 @@ def cron():
     P = Parser(url)
     df = P.parse_data_from_url()
     df_year_half = P.add_half(df)
-    return P.convert_to_json_all(df_year_half)
+
+    data = P.convert_to_json_all(df_year_half)
+    
+    with open("data/rates.json", "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
 
 if __name__ == "__main__":
     print(cron())
--- parser.py
+++ parser.py
@@ -13,12 +13,12 @@ class Parser():
             r.raise_for_status()
 
             df = pd.read_csv(StringIO(r.text),sep=";")
-            df.columns = ["data", "depo", "base_rate", "repo_rate", "loan"]
+            df.columns = ["date", "depo", "base_rate", "repo_rate", "loan"]
             
             # Удалил служебные даные таблицы
             df = df.iloc[:-3]
 
-            df["data"] = pd.to_datetime(df["data"],format="%d.%m.%Y")
+            df["date"] = pd.to_datetime(df["date"],format="%d.%m.%Y")
 
             
             return df
@@ -30,7 +30,7 @@ class Parser():
     @staticmethod
     def convert_to_json_all(df):
         try:
-            df["data"] = df["data"].dt.strftime("%d.%m.%Y")
+            df["date"] = df["date"].dt.strftime("%d.%m.%Y")
             array = df.to_dict(orient="records")
             array_json = json.dumps(array)
             
@@ -43,10 +43,10 @@ class Parser():
     def add_half(df):
         try:
             df["half"] = (
-                df["data"].dt.year.astype(str)
+                df["date"].dt.year.astype(str)
                 + ": "
                 + (
-                    df["data"].dt.month <= 6
+                    df["date"].dt.month <= 6
                 ).map({
                     True: "H1",
                     False: "H2"
